620c34bebc148c37a0a204392dccfd93297c448a
sahilaathia <athiasahila@gmail.com>
2026-02-13 10:04:35 +0700

    Implementing Vector Utility - Norm

--- individualprep-starter/src/main/java/com/example/individualprep/service/VectorUtility.java
+++ individualprep-starter/src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -79,7 +79,16 @@ public class VectorUtility {
     }
     
     public double norm(double[] v1) {
-        // TODO: Implement me properly!
-        return 0.0;
+        if (v1 == null) {
+            throw new IllegalArgumentException("Vector must not be null!");
+        }
+
+        double sum = 0;
+
+        for (int i = 0; i < v1.length; i++) {
+            sum += v1[i] * v1[i];
+        }
+
+        return Math.sqrt(sum);
     }
 }
